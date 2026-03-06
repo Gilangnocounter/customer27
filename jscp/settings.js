2fcad6f530bb708579cda9285df0507fdd4b582b
Langzc <115324908+Gilangnocounter@users.noreply.github.com>
2026-03-06 19:30:42 +0700

    Update settings.js

--- jscp/settings.js
+++ jscp/settings.js
@@ -9,7 +9,7 @@ let settings = {
     matrixText: 'HAPPYBIRTHDAY',
     matrixColor1: '#ff69b4', // Màu pink mẫu
     matrixColor2: '#ff1493', // Màu pink mẫu
-    sequence: 'HAPPY|BIRTHDAY|TO|RAJA|INTAN|IRDYANA|❤',
+    sequence: 'HAPPY|BIRTHDAY|TO|RAJA|INTAN|IRDYNA|❤',
     sequenceColor: '#ff69b4', // Màu pink mẫu
     gift: './image/happy3.gif',
     enableBook: true,
